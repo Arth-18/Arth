c4f7157ab2018619a60aa2e6f635f3153dd271bd
Arth-18 <149805368+Arth-18@users.noreply.github.com>
2023-11-12 20:13:48 -0500

    script.js

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector('.slider');
+    const images = document.querySelectorAll('.slider img');
+    const surpriseButton = document.getElementById('surpriseButton');
+    const fireworks = document.getElementById('fireworks');
 
-function showSlide(n) {
-    slides.forEach((slide) => {
-        slide.style.display = 'none';
+    let currentImage = 0;
+
+    function showImage(index) {
+        images[currentImage].style.opacity = 0;
+        images[index].style.opacity = 1;
+        currentImage = index;
+    }
+
+    function slide() {
+        const nextImage = (currentImage + 1) % images.length;
+        showImage(nextImage);
+    }
+
+    setInterval(slide, 5000); // Change image every 5 seconds
+
+    surpriseButton.addEventListener('click', function () {
+        const message = document.getElementById('message');
+        message.innerText = "Surprise! 🎉 Happy New Year!";
+        showImage(0); // Show the first image
+
+        // Trigger fireworks animation
+        fireworks.style.display = 'block';
+        fireworks.style.animation = 'fireworks 5s ease-in-out forwards';
+
+        setTimeout(function () {
+            message.innerText = "Even though I'm far away, my warm wishes are with you.";
+            fireworks.style.display = 'none';
+        }, 7000); // Reset the message and hide fireworks after 7 seconds
     });
-
-    slides[n].style.display = 'block';
-}
-
-function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-}
-
-// Initially, display the first slide
-showSlide(currentSlide);
-
-// Automatically move to the next slide every 3 seconds
-setInterval(nextSlide, 3000);
+});
